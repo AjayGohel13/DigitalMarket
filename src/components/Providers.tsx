983e987defdfa453c465f3f30e2f577bca1bcd06
AjayGohel13 <167311130+AjayGohel13@users.noreply.github.com>
2024-06-04 23:18:32 +0530

    Update Providers.tsx

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -14,7 +14,7 @@ const Providers = ({ children }: PropsWithChildren) => {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: `http://localhost:3000/api/trpc`,
+          url: `${process.env.NEXT_APP_URL}/api/trpc`,
           fetch(url, options) {
             return fetch(url, {
               ...options,
